b6404766e1ceab803168316938a5322a17a66f84
AousAlhajjaj <63973537+AousAlhajjaj@users.noreply.github.com>
2020-05-31 20:42:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// question 1
+
+var petArray = ["Cat", "Cow", "Dog"];
+
+
+
+
+
+// question 2
+
+console.log(petArray[1]);
+
+
+
+
+
+// question 3
+
+petArray.push("sheep");
+
+
+
+
+//question 4
+
+console.log(petArray.length);
+
+
+
+
+// question 5
+
+var catObject = {
+	name: "Aous",
+	colour: "Black",
+	age: 31
+};
+
+
+
+
+// question 6
+
+console.log(catObject.age);
+
+
+
+
+
+//Question 7.
+
+var catArray = [
+    {
+        name: "Aous",
+        colour: "Black",
+        age: 31
+    }
+];
+
+
+
+//Question 8
+
+for (var i = 0; i < catArray.length; i++) {
+ console.log(catArray[i].name);
+ console.log(catArray[i].colour);
+ console.log(catArray[i].age);
+}
+    
+
+
+
+//Question 9.
+
+function logToConsole(Monky) {
+    console.log(Monky);
+}
+
+
+
+
+//Question 10.
+
+logToConsole("donkey");
